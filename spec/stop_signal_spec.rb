e572ae6e34c79372c2ecb9d5b75114e4e1d2962d
Benjamin Oakes <boakes@doximity.com>
2023-01-06 17:58:16 -0600

    Build fake_job in "Arrange" step of ArrangeActAssert

--- spec/stop_signal_spec.rb
+++ spec/stop_signal_spec.rb
@@ -4,29 +4,29 @@ require "spec_helper"
 
 RSpec.describe SuperSpreader::StopSignal do
   it "has a lifecycle that allows stopping a job" do
+    fake_job = Class.new do
+      extend SuperSpreader::StopSignal
+    end
+
     # Default
-    expect(StopSignalFakeJob.stopped?).to eq(false)
+    expect(fake_job.stopped?).to eq(false)
 
-    StopSignalFakeJob.stop!
+    fake_job.stop!
 
-    expect(StopSignalFakeJob.stopped?).to eq(true)
-
-    # Idempotent
-    StopSignalFakeJob.stop!
-
-    expect(StopSignalFakeJob.stopped?).to eq(true)
-
-    StopSignalFakeJob.go!
-
-    expect(StopSignalFakeJob.stopped?).to eq(false)
+    expect(fake_job.stopped?).to eq(true)
 
     # Idempotent
-    StopSignalFakeJob.go!
+    fake_job.stop!
 
-    expect(StopSignalFakeJob.stopped?).to eq(false)
-  end
+    expect(fake_job.stopped?).to eq(true)
 
-  class StopSignalFakeJob
-    extend SuperSpreader::StopSignal
+    fake_job.go!
+
+    expect(fake_job.stopped?).to eq(false)
+
+    # Idempotent
+    fake_job.go!
+
+    expect(fake_job.stopped?).to eq(false)
   end
 end
